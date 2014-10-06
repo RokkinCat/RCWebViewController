21f2a260a6012812e1fcbfbdadc14ea7d97a5aa1
Josh Holtz <josh@rokkincat.com>
2014-10-05 19:34:40 -0500

    Initial commit

---
+++ RCWebViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RCWebViewController
+//
+//  Created by Josh Holtz on 10/5/14.
+//  Copyright (c) 2014 RokkinCat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RCWebViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RCWebViewController
+//
+//  Created by Josh Holtz on 10/5/14.
+//  Copyright (c) 2014 RokkinCat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
